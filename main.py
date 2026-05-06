5eb9070edb30d8f71857ab06cee0e22465a7cd05
monirul8888 <monirul429588@gmail.com>
2026-05-06 23:24:49 +0600

    Added About

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"msg" : "Welcome To FastAPI Learning"}
+
+@app.get("/about")
+def about():
+    return{"Name" : "Md. Monirul Islam",
+           "Department": "CSE"}
