76c4900a5f32cf8757ce701377668a592933efcd
monirul8888 <monirul429588@gmail.com>
2026-05-07 23:38:58 +0600

    Database Added Successfully

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import psycopg2
+import time
+
+from psycopg2.extras import RealDictCursor 
 
 app = FastAPI()
 
@@ -8,6 +12,29 @@ class Student(BaseModel):
     name : str
     id : int
     dept : str
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host = "localhost",
+            database = "university",
+            user = "postgres",
+            password = "1234",
+            cursor_factory = RealDictCursor
+        )
+
+        cursor = conn.cursor()
+
+        print("Successfully Connected To The Database")
+        break
+    except Exception as error:
+        print("Database Connection Failed")
+        print("Error : ", error)
+        time.sleep(2)
+
+
+
 
 
 @app.get("/")
