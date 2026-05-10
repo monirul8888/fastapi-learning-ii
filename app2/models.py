b095219abc0f4a1a9e2d0ea18d113e2e6cac1701
monirul8888 <monirul429588@gmail.com>
2026-05-10 21:12:38 +0600

    Get All Students

--- app2/models.py
+++ app2/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Float
 from . database import Base
 
 class Student2(Base):
-    __tablename__ = "students2"
+    __tablename__ = "students3"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     dept = Column(String, nullable=False)
