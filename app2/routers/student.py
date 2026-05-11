697f04952a16431f78995c1a71057b58c2703c8d
Monirul <monirul429588@gmail.com>
2026-05-11 12:22:54 +0600

    Added Token

--- app2/routers/student.py
+++ app2/routers/student.py
@@ -1,8 +1,11 @@
-from .. import models, schemas
-from ..database import get_db
-from fastapi import Depends, APIRouter
-from sqlalchemy.orm import Session
 from typing import List
+
+from fastapi import Depends, APIRouter, HTTPException, status
+from sqlalchemy.orm import Session
+
+from .. import models, schemas, oauth2
+from ..database import get_db
+
 
 router = APIRouter(
     prefix="/students",
@@ -10,22 +13,42 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List [schemas.StudentResponse])
-def get_student(db : Session = Depends(get_db)):
-    st = db.query(models.Student2).all()
-    return st
+@router.get("/", response_model=List[schemas.StudentResponse])
+def get_students(
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(oauth2.get_current_user)
+):
+    students = db.query(models.Student2).all()
+    return students
 
 
 @router.get("/{id}", response_model=schemas.StudentResponse)
-def get_student(id : int, db : Session = Depends(get_db), ):
+def get_student_by_id(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(oauth2.get_current_user)
+):
     student = db.query(models.Student2).filter(models.Student2.id == id).first()
+
+    if not student:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Student with id {id} not found"
+        )
+
     return student
 
 
 @router.post("/", response_model=schemas.StudentResponse)
-def create_student(student : schemas.CreateStudent, db : Session = Depends(get_db)):
-    new_st = models.Student2(**student.model_dump())
-    db.add(new_st)
+def create_student(
+    student: schemas.CreateStudent,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(oauth2.get_current_user)
+):
+    new_student = models.Student2(**student.model_dump())
+
+    db.add(new_student)
     db.commit()
-    db.refresh(new_st)
-    return new_st
+    db.refresh(new_student)
+
+    return new_student
--- app2/schemas.py
+++ app2/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 class CreateStudent(BaseModel):
     name : str
@@ -27,7 +28,15 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
-    
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[int] = None
+
 
 
 
