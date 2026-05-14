dbff94542a15ac12c9cdd3264ecd845c557a0ee1
Monirul <monirul429588@gmail.com>
2026-05-14 10:22:07 +0600

    Added Post

--- app3/main.py
+++ app3/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from . database import get_db
+from . database import get_db, Base, engine
+from .schemas import StudentCreate, StudentResponse
+from . import models
 
 app = FastAPI()
+Base.metadata.create_all(bind = engine)
 
-@app.get("/about")
+
+@app.get("/about/")
 def About():
     return {"msg" : "Monirul Islam"}
 
 @app.get("/")
 def home(db : Session = Depends(get_db)):
     return {"msg" : "Databased Connected"}
+
+
+@app.post("/students/", response_model=StudentResponse)
+def create_student(student : StudentCreate,
+                   db: Session = Depends(get_db)):
+    new_student = models.Student(**student.model_dump())
+
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+
+    return new_student
